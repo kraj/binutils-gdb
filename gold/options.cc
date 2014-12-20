081ff0b8a64fa33a2fbd8b900f31228138400438
H.J. Lu <hjl.tools@gmail.com>
2014-12-20 05:45:51 -0800

    Treat .eh_frame section before crtbeginT as normal input
    Force the exception frame section from input files before the crtbeginT
    file to be handled as an ordinary input section if we aren't creating
    the exception frame header.  If we don't do this, we won't correctly
    handle the special marker symbol in the exception frame section in the
    crtbeginT file.
    
    	PR gold/14675
    	* ehframe.cc (Eh_frame::add_ehframe_input_section): Force the
    	exception frame section from input files if it can't be
    	optimized.
    	(Eh_frame::add_ehframe_input_section<32, false>): Updated.
    	(Eh_frame::add_ehframe_input_section<32, true>): Likewise.
    	(Eh_frame::add_ehframe_input_section<64, false>): Likewise.
    	(Eh_frame::add_ehframe_input_section<64, true>): Likewise.
    	* ehframe.h (Eh_frame::add_ehframe_input_section): Add a
    	bool parameter to indicate if the exception frame section
    	can be optimized.
    	* layout.cc (Layout::Layout): Initialize optimize_ehframe_ to
    	!has_crtbeginT.
    	(Layout::layout_eh_frame): Pass this->optimize_ehframe_ to
    	Eh_frame::add_ehframe_input_section.
    	(Layout::make_eh_frame_section): Set this->optimize_ehframe_
    	to true when processing the crtbeginT file if it is on command
    	line.
    	(Layout::match_file_name (const char*, const char*)): New.
    	(Layout::match_file_name(const Relobj*, const char*): Use it.
    	* layout.h (Layout::Layout): Add has_crtbeginT.
    	(Layout::match_file_name (const char*, const char*)): New.
    	(Layout): Add an optimize_ehframe_ member.
    	* main.cc (main): Pass command_line.has_crtbeginT() to layout.
    	* options.cc: Include "layout.h".
    	(Input_arguments::add_file): Set this->has_crtbeginT_ to true
    	if there is a crtbeginT file and the last one is a crti file.
    	* options.h (Input_arguments::Input_arguments): Initialize
    	has_crtbeginT_ and last_is_crti_ to false.
    	(Input_arguments::has_crtbeginT): New function.
    	(Input_arguments::has_crtbeginT_): New bool member.
    	(Input_arguments::last_is_crti_): Likewise.
    	(Command_line::has_crtbeginT): New function.

--- gold/options.cc
+++ gold/options.cc
@@ -38,6 +38,7 @@
 #include "script.h"
 #include "target-select.h"
 #include "options.h"
+#include "layout.h"
 #include "plugin.h"
 
 namespace gold
@@ -1346,6 +1347,19 @@ Input_arguments::add_file(Input_file_argument& file)
       return this->input_argument_list_.back().lib()->add_file(file);
     }
   this->input_argument_list_.push_back(Input_argument(file));
+  if (!this->has_crtbeginT_)
+    {
+      // Set has_crtbeginT_ to true only if the last one is a crti file.
+      if (Layout::match_file_name(file.name(), "crti"))
+	this->last_is_crti_ = true;
+      else
+	{
+	  if (this->last_is_crti_
+	      && Layout::match_file_name(file.name(), "crtbeginT"))
+	    this->has_crtbeginT_ = true;
+	  this->last_is_crti_ = false;
+	}
+    }
   return this->input_argument_list_.back();
 }
 
