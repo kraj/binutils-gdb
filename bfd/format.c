dbe012eabd23ec307b2e3dfadaa90d4190bfb479
H.J. Lu <hjl.tools@gmail.com>
2015-02-08 17:53:24 -0800

    Don't check the plugin target twice
    If the plugin target is explicitly specified when a BFD file is opened,
    don't check it twice.
    
    	* format.c: Include "plugin-api.h" and "plugin.h" if
    	BFD_SUPPORTS_PLUGINS is defined.
    	(bfd_check_format_matches): Don't check the plugin target twice
    	if the plugin target is explicitly specified.

--- bfd/format.c
+++ bfd/format.c
@@ -46,6 +46,10 @@ SUBSECTION
 #include "sysdep.h"
 #include "bfd.h"
 #include "libbfd.h"
+#if BFD_SUPPORTS_PLUGINS
+#include "plugin-api.h"
+#include "plugin.h"
+#endif
 
 /* IMPORT from targets.c.  */
 extern const size_t _bfd_target_vector_entries;
@@ -237,9 +241,6 @@ bfd_boolean
 bfd_check_format_matches (bfd *abfd, bfd_format format, char ***matching)
 {
   extern const bfd_target binary_vec;
-#if BFD_SUPPORTS_PLUGINS
-  extern const bfd_target plugin_vec;
-#endif
   const bfd_target * const *target;
   const bfd_target **matching_vector = NULL;
   const bfd_target *save_targ, *right_targ, *ar_right_targ, *match_targ;
@@ -325,6 +326,13 @@ bfd_check_format_matches (bfd *abfd, bfd_format format, char ***matching)
 	  || (!abfd->target_defaulted && *target == save_targ))
 	continue;
 
+#if BFD_SUPPORTS_PLUGINS
+      /* If the plugin target is explicitly specified when a BFD file
+	 is opened, don't check it twice.  */
+      if (bfd_plugin_specified_p () && bfd_plugin_target_p (*target))
+	continue;
+#endif
+
       /* If we already tried a match, the bfd is modified and may
 	 have sections attached, which will confuse the next
 	 _bfd_check_format call.  */
@@ -351,7 +359,7 @@ bfd_check_format_matches (bfd *abfd, bfd_format format, char ***matching)
 	     lowest priority; objects both handled by a plugin and
 	     with an underlying object format will be claimed
 	     separately by the plugin.  */
-	  if (*target == &plugin_vec)
+	  if (bfd_plugin_target_p (*target))
 	    match_priority = (*target)->match_priority;
 #endif
 
