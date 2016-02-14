1bfce9b2fb66cb2dc4b4e45428e181ab48e7b392
Khem Raj <raj.khem@gmail.com>
2016-02-14 17:06:19 +0000

    Add support for Netlogic XLP
    Patch From: Nebu Philips <nphilips@netlogicmicro.com>
    
    Using the mipsisa64r2nlm target, add support for XLP from
    Netlogic. Also, update vendor name to NLM wherever applicable.
    
    Use 0x00000080 for INSN_XLP, the value 0x00000040 has already been
    assigned to INSN_OCTEON3
    
    Upstream-Status: Pending
    
    Signed-off-by: Khem Raj <raj.khem@gmail.com>
    Signed-off-by: Baoshan Pang <baoshan.pang@windriver.com>
    Signed-off-by: Mark Hatle <mark.hatle@windriver.com>

--- bfd/elfxx-mips.c
+++ bfd/elfxx-mips.c
@@ -6980,6 +6980,9 @@ _bfd_elf_mips_mach (flagword flags)
     case E_MIPS_MACH_IAMR2:
       return bfd_mach_mips_interaptiv_mr2;
 
+    case E_MIPS_MACH_XLP:
+      return bfd_mach_mips_xlp;
+
     default:
       switch (flags & EF_MIPS_ARCH)
 	{
@@ -12339,6 +12342,10 @@ mips_set_isa_flags (bfd *abfd)
       val = E_MIPS_ARCH_64R2 | E_MIPS_MACH_OCTEON2;
       break;
 
+	case bfd_mach_mips_xlp:
+	  val = E_MIPS_ARCH_64R2 | E_MIPS_MACH_XLP;
+	  break;
+
     case bfd_mach_mipsisa32:
       val = E_MIPS_ARCH_32;
       break;
@@ -14428,6 +14435,7 @@ static const struct mips_mach_extension mips_mach_extensions[] =
   { bfd_mach_mips_gs264e, bfd_mach_mips_gs464e },
   { bfd_mach_mips_gs464e, bfd_mach_mips_gs464 },
   { bfd_mach_mips_gs464, bfd_mach_mipsisa64r2 },
+  { bfd_mach_mips_xlp, bfd_mach_mipsisa64r2 },
 
   /* MIPS64 extensions.  */
   { bfd_mach_mipsisa64r2, bfd_mach_mipsisa64 },
