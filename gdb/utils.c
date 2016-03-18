b0661615cc989862b198a2fcb8fd585c52d5b05a
Pedro Alves <palves@redhat.com>
2016-03-18 19:15:55 +0000

    Don't set immediate_quit in prompt_for_continue
    immediate_quit used to be necessary back when prompt_for_continue used
    blocking fread, but nowadays it uses gdb_readline_wrapper, which is
    implemented in terms of a nested event loop, which already knows how
    to react to SIGINT:
    
     #0  throw_it (reason=RETURN_QUIT, error=GDB_NO_ERROR, fmt=0x9d6d7e "Quit", ap=0x7fffffffcb88)
         at /home/pedro/gdb/mygit/cxx-convertion/src/gdb/common/common-exceptions.c:324
     #1  0x00000000007bab5d in throw_vquit (fmt=0x9d6d7e "Quit", ap=0x7fffffffcb88) at /home/pedro/gdb/mygit/cxx-convertion/src/gdb/common/common-exceptions.c:366
     #2  0x00000000007bac9f in throw_quit (fmt=0x9d6d7e "Quit") at /home/pedro/gdb/mygit/cxx-convertion/src/gdb/common/common-exceptions.c:385
     #3  0x0000000000773a2d in quit () at /home/pedro/gdb/mygit/cxx-convertion/src/gdb/utils.c:1039
     #4  0x000000000065d81b in async_request_quit (arg=0x0) at /home/pedro/gdb/mygit/cxx-convertion/src/gdb/event-top.c:893
     #5  0x000000000065c27b in invoke_async_signal_handlers () at /home/pedro/gdb/mygit/cxx-convertion/src/gdb/event-loop.c:949
     #6  0x000000000065aeef in gdb_do_one_event () at /home/pedro/gdb/mygit/cxx-convertion/src/gdb/event-loop.c:280
     #7  0x0000000000770838 in gdb_readline_wrapper (prompt=0x7fffffffcd40 "---Type <return> to continue, or q <return> to quit---")
         at /home/pedro/gdb/mygit/cxx-convertion/src/gdb/top.c:873
    
    The need for the QUIT in stdin_event_handler is then exposed by the
    gdb.base/double-prompt-target-event-error.exp test, which has:
    
    	# We're now stopped in a pagination query while handling a
    	# target event (printing where the program stopped).  Quitting
    	# the pagination should result in only one prompt being
    	# output.
    	send_gdb "\003p 1\n"
    
    Without that change we'd get:
    
     Continuing.
     ---Type <return> to continue, or q <return> to quit---PASS: gdb.base/double-prompt-target-event-error.exp: ctrlc target event: continue: continue to pagination
     ^CpQuit
     (gdb)  1
     Undefined command: "1".  Try "help".
     (gdb) PASS: gdb.base/double-prompt-target-event-error.exp: ctrlc target event: continue: first prompt
     ERROR: Undefined command "".
     UNRESOLVED: gdb.base/double-prompt-target-event-error.exp: ctrlc target event: continue: no double prompt
    
    Vs:
    
     Continuing.
     ---Type <return> to continue, or q <return> to quit---PASS: gdb.base/double-prompt-target-event-error.exp: ctrlc target event: continue: continue to pagination
     ^CQuit
     (gdb) p 1
     $1 = 1
     (gdb) PASS: gdb.base/double-prompt-target-event-error.exp: ctrlc target event: continue: first prompt
     PASS: gdb.base/double-prompt-target-event-error.exp: ctrlc target event: continue: no double prompt
    
    gdb/ChangeLog:
    yyyy-mm-dd  Pedro Alves  <palves@redhat.com>
    
    	* event-top.c (stdin_event_handler): Call QUIT;
    	(prompt_for_continue): Don't run with immediate_quit set.

--- gdb/utils.c
+++ gdb/utils.c
@@ -1836,9 +1836,6 @@ prompt_for_continue (void)
      beyond the end of the screen.  */
   reinitialize_more_filter ();
 
-  immediate_quit++;
-  QUIT;
-
   /* We'll need to handle input.  */
   target_terminal_ours ();
 
@@ -1866,7 +1863,6 @@ prompt_for_continue (void)
 	throw_quit ("Quit");
       xfree (ignore);
     }
-  immediate_quit--;
 
   /* Now we have to do this again, so that GDB will know that it doesn't
      need to save the ---Type <return>--- line at the top of the screen.  */
