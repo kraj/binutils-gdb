533c9fe36683102b1f0b8655d83393fa3d456651
Antoine Tremblay <antoine.tremblay@ericsson.com>
2016-06-16 23:46:53 +0100

    Test for detach problem

---
+++ gdb/testsuite/gdb.threads/detach-gone-thread-fork.c
@@ -0,0 +1,71 @@
+/* This testcase is part of GDB, the GNU debugger.
+
+   Copyright 2016 Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+#include <pthread.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+pthread_barrier_t barrier;
+
+#define NTHREADS 256
+
+int globalvar = 1;
+
+void *
+child_function (void *arg)
+{
+  pthread_barrier_wait (&barrier);
+  _exit (0);
+}
+
+int
+main ()
+{
+  pthread_t threads[NTHREADS];
+  int res;
+  int i;
+  pid_t pid;
+
+  pid = fork ();
+  if (pid == -1)
+    return 1;
+
+  if (pid == 0)
+    {
+      pthread_barrier_init (&barrier, NULL, NTHREADS + 1);
+
+      for (i = 0; i < NTHREADS; i++)
+	res = pthread_create (&threads[i], NULL, child_function, NULL);
+      pthread_barrier_wait (&barrier);
+      exit (0);
+    }
+  else
+    {
+      int status, ret;
+      ret = waitpid (pid, &status, 0);
+      if (ret == -1)
+	exit (1);
+      if (WIFEXITED(status)) {
+	printf("exited, status=%d\n", WEXITSTATUS(status));
+      }
+      if (WIFEXITED (status) || WIFSIGNALED (status))
+	exit (0);
+      else
+	exit (1);
+    }
+}
