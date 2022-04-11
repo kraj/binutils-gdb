1cf48ecc79a6c15af102d76f41c349dbf6a85efd
Markus Metzger <markus.t.metzger@intel.com>
2022-04-11 16:44:36 +0200

    gdb, python: use gdbarch_iterate_over_objfiles_in_search_order
    The implementation of gdb.lookup_objfile() iterates over all objfiles and
    compares their name or build id to the user-provided search string.
    
    This will cause problems when supporting linker namespaces as the first
    objfile in any namespace will be found.  Instead, use
    gdbarch_iterate_over_objfiles_in_search_order to only consider the
    namespace of gdb.current_objfile() for the search, which defaults to the
    initial namespace when gdb.current_objfile() is None.

--- gdb/python/python.c
+++ gdb/python/python.c
@@ -1573,11 +1573,8 @@ gdbpy_progspaces (PyObject *unused1, PyObject *unused2)
 
 
 
-/* The "current" objfile.  This is set when gdb detects that a new
-   objfile has been loaded.  It is only set for the duration of a call to
-   gdbpy_source_objfile_script and gdbpy_execute_objfile_script; it is NULL
-   at other times.  */
-static struct objfile *gdbpy_current_objfile;
+/* See python.h.  */
+struct objfile *gdbpy_current_objfile;
 
 /* Set the current objfile to OBJFILE and then read FILE named FILENAME
    as Python code.  This does not throw any errors.  If an exception
